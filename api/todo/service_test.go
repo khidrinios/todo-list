1a0edb23d504795e42f0def6aeba177a48f89def
Ibrahim Khidr <ibrahim.khidr@jumia.com>
2021-12-01 14:38:03 +0200

    add tests and test coverage

--- api/todo/service_test.go
+++ api/todo/service_test.go
@@ -3,24 +3,70 @@ package todo_test
 import (
 	"fmt"
 	"khidr/todo/api/todo"
-	i "khidr/todo/interfaces/api/todo"
+	todoI "khidr/todo/interfaces/api/todo"
 	mocks "khidr/todo/mocks/interfaces/api/todo"
+	"khidr/todo/models"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type serviceMocks struct {
+	repoMock *mocks.Repository
+}
+
+func setupServiceMocks() (*todo.Service, serviceMocks) {
+	repoMock := new(mocks.Repository)
+	s := todo.NewService(repoMock)
+	return s, serviceMocks{repoMock}
+}
+
 func TestService_CreateTodo(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		mockService := new(mocks.Service)
-		request := i.CreateRequest{Title: "Todo 1"}
-		mockService.On("CreateTodo", request).Return(&i.TodoIdResult{
-			Id: 1,
-		}, nil)
-		res, err := mockService.CreateTodo(request)
-		assert.NotNil(t, res)
+		s, m := setupServiceMocks()
+		title := "Todo 1"
+		from, to := todo.GetBeginningAndEndTime(time.Now())
+		queryReq := todoI.QueryTodosRequest{
+			Title: &title,
+			From:  &from,
+			To:    &to,
+		}
+		m.repoMock.On("QueryTodos", queryReq).Return([]models.Todo{}, nil)
+		id := 1
+		todoModel := models.Todo{
+			Title: title,
+		}
+		m.repoMock.On("Create", todoModel).Return(&id, nil)
+		request := todoI.CreateRequest{
+			Title: title,
+		}
+		res, err := s.Create(request)
+		assert.Equal(t, id, *res)
 		assert.Nil(t, err)
+	})
+
+	t.Run("error - existing todo on the same day", func(t *testing.T) {
+		s, m := setupServiceMocks()
+		title := "Todo 1"
+		from, to := todo.GetBeginningAndEndTime(time.Now())
+		queryReq := todoI.QueryTodosRequest{
+			Title: &title,
+			From:  &from,
+			To:    &to,
+		}
+		todoModel := models.Todo{
+			Title: title,
+		}
+		m.repoMock.On("QueryTodos", queryReq).Return([]models.Todo{todoModel}, nil)
+		request := todoI.CreateRequest{
+			Title: title,
+		}
+		res, err := s.Create(request)
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
+		_, ok := err.(todo.ValidationError)
+		assert.True(t, ok)
 	})
 }
 
